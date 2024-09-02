c110ec3aa99ec46c3a209ea5005c7192d231a1aa
Lukas Gade Ravnsborg <lravns20@student.aau.dk>
2024-09-02 12:43:20 +0200

    Part 1 of 4

---
+++ Assets/Assets/ControlPanel/ControlPanel/ButtonClick.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonClick : MonoBehaviour
+{
+    public int buttonID; // Identifikator for hver knap
+    //public Animator animator;
+
+    void Start ()
+    {
+      //  animator = GetComponent<Animator>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            ButtonClicked(buttonID); // Kald funktionen med knappens ID
+        }
+    }
+
+    void ButtonClicked(int id)
+    {
+        // Udfør handling baseret på knappens ID
+        switch (id)
+        {
+            case 1:
+                // Handling for knap 1
+                Debug.Log("Disable Sound");
+                //animator.SetTrigger("ChangeColorTrigger");
+                GameManager.instance.DisableAudioMixer();
+                break;
+            case 2:
+                // Handling for knap 2
+                Debug.Log("Disable NPC");
+                //  animator.SetTrigger("ChangeColorTrigger");
+                GameManager.instance.DisableNPC();
+                break;
+            case 3:
+                // Handling for knap 3
+                Debug.Log("Start Game");
+               // animator.SetTrigger("ChangeColorTrigger");
+                GameManager.instance.StartGame();
+
+                break;
+            default:
+                Debug.Log("Unhandled button click");
+                break;
+        }
+    }
+
+}
