66d13248c4180729c0d60225745b3730744df1b9
FreyaGadeMEA20 <lravns20@student.aau.dk>
2024-09-18 15:01:34 +0200

    the bus on the wheels drve ruond ruond ruond

--- Assets/Scripts/VehicleMovement/VehicleMovement.cs
+++ Assets/Scripts/VehicleMovement/VehicleMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Movement{
@@ -29,13 +30,20 @@ namespace Movement{
         [SerializeField] float acceleration = 1000f;
         [SerializeField] float maxSteering = 20f;
         [SerializeField] float breakingForce = 300f;
-        [Range(0,1)][SerializeField] float wayPointAcc = 0f;
-        [Range(0,1)][SerializeField] float wayPointSteer = 0f;
+        /* [Range(0,1)] */[SerializeField] float wayPointAcc = 0f;
+        [Range(-1,1)][SerializeField] float wayPointSteer = 0f;
         [SerializeField] bool WaypointBreak = false;
 
-        float currentAcceleration = 0f;
-        float currentBreakForce = 0f;
-        float currentSteering = 0f;
+        [SerializeField] float currentAcceleration = 0f;
+        [SerializeField] float currentBreakForce = 0f;
+        [SerializeField] float currentSteering = 0f;
+
+        public float[] GearRatio;
+	    public int CurrentGear = 0;
+        public float EngineTorque = 600.0f;
+	    public float MaxEngineRPM = 3000.0f;
+	    public float MinEngineRPM = 1000.0f;
+        float EngineRPM = 0.0f;
 
         [Header("NAVIGATION AND COLLISSION")]
         Vector3 destination;
@@ -44,23 +52,28 @@ namespace Movement{
             set{ReachedDestination = value;}
         }
         public bool ReachedDestination = false;
-        Waypoint currentWaypoint;
-        [SerializeField] GameObject waypointDetector;
-        [SerializeField] float safeDistance = 2f;
+        RouteManager routeManager;
+        [SerializeField] Waypoint currentWaypoint;
         
+        int direction = 1;
+        Rigidbody rb;
 
         // Start is called before the first frame update
         void Start()
         {
             //waypoints = GameObject.Find("Vehicle Route Manager").GetComponent<RouteManager>();
             //carSpawner = GameObject.Find("Spawn Manager").GetComponent<CarSpawner>();
-
+            rb = GetComponent<Rigidbody>();
+            rb.centerOfMass = new Vector3 (rb.centerOfMass.x, -1.5f, rb.centerOfMass.z);
             //currentRoute = waypoints.routes[currentRouteIndex];
             if (entityType == EntityTypes.Bus){
                 busController = this.GetComponent<BusController>();
             } 
 
-            StartCoroutine(MovementSM());
+            routeManager = GetComponent<RouteManager>();
+            currentWaypoint = routeManager.currentWaypoint;
+
+            //StartCoroutine(MovementSM());
         }
 
         private IEnumerator MovementSM(){
@@ -121,19 +134,56 @@ namespace Movement{
             // Current movement state keeps track of whether it is supposed to listen for waypoints, or be stopped
             switch(currentMovementState){
                 case MovementState.Moving:
+                    rb.drag = rb.velocity.magnitude / 250;
+                    NavigateTowardsWaypoint();
                     //MoveTowardsWaypoint();
                     //RotateTowardsWaypoint();
-                    
+
+                    EngineRPM = (rearWheels[0].rpm + rearWheels[1].rpm)/2 * GearRatio[CurrentGear];
+                    ShiftGears();
+
+                    //audio.pitch = Mathf.Abs(EngineRPM) + 1;
+                    // limit audio                    
                     break;
             }
             // After the program has checked where it is in accordance to its surroundings, it will apply the forces to the wheels
             Move();
         }
 
+        void  ShiftGears (){
+		// this funciton shifts the gears of the vehcile, it loops through all the gears, checking which will make
+		// the engine RPM fall within the desired range. The gear is then set to this "appropriate" value.
+		int AppropriateGear = CurrentGear;
+
+		if ( EngineRPM >= MaxEngineRPM ) {
+			for (int i= 0; i < GearRatio.Length; i ++ ) {
+				if ( rearWheels[0].rpm * GearRatio[i] < MaxEngineRPM ) {
+					AppropriateGear = i;
+					break;
+				}
+			}
+			
+			CurrentGear = AppropriateGear;
+		}
+		
+		if ( EngineRPM <= MinEngineRPM ) {
+			AppropriateGear = CurrentGear;
+			
+			for ( int j= GearRatio.Length - 1; j >= 0; j -- ) {
+				if ( rearWheels[0].rpm * GearRatio[j] > MinEngineRPM ) {
+					AppropriateGear = j;
+					break;
+				}
+			}
+			
+			CurrentGear = AppropriateGear;
+		}
+	}
+
         // Possible TODO: Seperate the forces, and change how the steering is applied to the vehicle
         public void ApplyForces(float speed, float steering, bool breaking){
-            currentAcceleration = (speed * wayPointAcc) * acceleration;
-            currentSteering = (steering * wayPointSteer) * maxSteering;
+            currentAcceleration = wayPointAcc * acceleration * EngineRPM;
+            currentSteering = wayPointSteer * maxSteering;
             
             currentBreakForce = (breaking || WaypointBreak) ? breakingForce : 0f;
         }
@@ -144,16 +194,50 @@ namespace Movement{
         public void Move(){
             // Apply forces to the front wheels
             foreach (WheelCollider wheel in frontWheels){
-                wheel.steerAngle = currentSteering;
+                wheel.steerAngle = maxSteering * wayPointSteer;
                 wheel.brakeTorque = currentBreakForce;
                 //wheel.motorTorque = currentAcceleration;
             }
             // Apply forces to the rear wheels
             foreach (WheelCollider wheel in rearWheels){
-                wheel.motorTorque = currentAcceleration;
+                wheel.motorTorque = EngineTorque / GearRatio[CurrentGear] * wayPointAcc;
                 wheel.brakeTorque = currentBreakForce;
             }
         }
+
+        void NavigateTowardsWaypoint (){
+		// now we just find the relative position of the waypoint from the car transform,
+		// that way we can determine how far to the left and right the waypoint is.
+		Vector3 RelativeWaypointPosition = transform.InverseTransformPoint( new Vector3(
+                                                                                    currentWaypoint.GetPosition().x, 
+                                                                                    transform.position.y, 
+                                                                                    currentWaypoint.GetPosition().z ) );
+		
+		// by dividing the horizontal position by the magnitude, we get a decimal percentage of the turn angle that we can use to drive the wheels
+		wayPointSteer = RelativeWaypointPosition.x / RelativeWaypointPosition.magnitude;
+		
+		// now we do the same for torque, but make sure that it doesn't apply any engine torque when going around a sharp turn...
+		if ( Mathf.Abs( wayPointSteer ) < 0.5f ) {
+			wayPointAcc = RelativeWaypointPosition.z / RelativeWaypointPosition.magnitude - Mathf.Abs( wayPointSteer );
+		}else{
+			wayPointAcc = 0.0f;
+		}
+		
+		// this just checks if the car's position is near enough to a waypoint to count as passing it, if it is, then change the target waypoint to the
+		// next in the list.
+		if ( RelativeWaypointPosition.magnitude < 20 ) {
+            if(direction == 1){
+                currentWaypoint = currentWaypoint.nextWaypoint;
+			} else if (direction == -1){
+                currentWaypoint = currentWaypoint.previousWaypoint;
+            }
+
+			/* if ( currentWaypoint >= waypoints.Count ) {
+				currentWaypoint = 0;
+			} */
+		}
+		
+	}
 
         public void SetDestination(Vector3 destination){
             this.destination = destination;
