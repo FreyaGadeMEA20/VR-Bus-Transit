ce473bc51ef6c9e5acc9f75a2c6f8d0d021170c5
FreyaGadeMEA20 <lravns20@student.aau.dk>
2024-10-21 11:35:58 +0200

    Clean up in aisle Pedestrian

--- Assets/Editor/PedestrianWaypointManagerWindow.cs
+++ Assets/Editor/PedestrianWaypointManagerWindow.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using Movement;
 
 public class PedestrianWaypointManagerWindow : EditorWindow
 {
