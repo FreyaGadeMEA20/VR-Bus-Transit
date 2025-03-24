4a0ab294228509e571abc66813fc8defaf2b26ed
NichlasLaursen <72730726+NichlasLaursen@users.noreply.github.com>
2025-03-24 14:52:17 +0100

    Basic Script Setup

--- Assets/Scripts/Buttons/Level_High.cs
+++ Assets/Scripts/Buttons/Level_High.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Level_Low : MonoBehaviour
+public class Level_High : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Slider npcSlider;
+    public Slider ambienceSlider;
+    public Slider chatterSlider;
 
-    // Update is called once per frame
-    void Update()
+    public void SetHighLevel()
     {
-        
+        if (npcSlider != null)
+        {
+            npcSlider.value = 1.0f; // Set NPC slider to 100
+            Debug.Log("[Level_High] NPC slider set to 100.");
+        }
+
+        if (ambienceSlider != null)
+        {
+            ambienceSlider.value = 1.0f; // Set Ambience slider to 1.0
+            Debug.Log("[Level_High] Ambience slider set to 1.0.");
+        }
+
+        if (chatterSlider != null)
+        {
+            chatterSlider.value = 1.0f; // Set Chatter slider to 1.0
+            Debug.Log("[Level_High] Chatter slider set to 1.0.");
+        }
     }
 }
+
+
--- Assets/Scripts/Buttons/Level_Medium.cs
+++ Assets/Scripts/Buttons/Level_Medium.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Level_Low : MonoBehaviour
+public class Level_Medium : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Slider npcSlider;
+    public Slider ambienceSlider;
+    public Slider chatterSlider;
 
-    // Update is called once per frame
-    void Update()
+    public void SetMediumLevel()
     {
-        
+        if (npcSlider != null)
+        {
+            npcSlider.value = 0.66f; // Set NPC slider to 66
+            Debug.Log("[Level_Medium] NPC slider set to 66.");
+        }
+
+        if (ambienceSlider != null)
+        {
+            ambienceSlider.value = 0.66f; // Set Ambience slider to 0.66
+            Debug.Log("[Level_Medium] Ambience slider set to 0.66.");
+        }
+
+        if (chatterSlider != null)
+        {
+            chatterSlider.value = 0.66f; // Set Chatter slider to 0.66
+            Debug.Log("[Level_Medium] Chatter slider set to 0.66.");
+        }
     }
 }
