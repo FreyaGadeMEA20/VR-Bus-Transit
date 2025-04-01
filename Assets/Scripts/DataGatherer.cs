8d45716692c3d167d9345741f091da827600cd62
Freya Gade Ravnsborg <lravns20@student.aau.dk>
2025-04-01 09:41:31 +0200

    Changed to streamwriter = works
    Data gets saved to "Android/data/com.MultisensoryExperienceLab.VRBusTransit/files" and then as an incremental data file

--- Assets/Scripts/DataGatherer.cs
+++ Assets/Scripts/DataGatherer.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class DataGatherer : MonoBehaviour
@@ -8,6 +9,7 @@ public class DataGatherer : MonoBehaviour
     string fileName = "";
     string[] rowDataTemp = new string[6];
     string rowData;
+    StreamWriter streamWriter;
 
     void Awake()
     {
@@ -21,11 +23,12 @@ public class DataGatherer : MonoBehaviour
             Destroy(gameObject);
         }   
     }
+
     // Start is called before the first frame update
     void Start()
     {
-        string folderPath = Application.dataPath;
-        string baseFileName = "Data/TestData";
+        string folderPath = Application.persistentDataPath; // Use persistentDataPath for Meta Quest compatibility
+        string baseFileName = "TestData";
         string extension = ".csv";
         int fileNumber = 1;
 
@@ -33,23 +36,36 @@ public class DataGatherer : MonoBehaviour
         {
             fileName = $"{folderPath}/{baseFileName}{fileNumber}{extension}";
             fileNumber++;
-            Debug.Log(fileName);
-        } while (System.IO.File.Exists(fileName));
+        } while (File.Exists(fileName));
+
+        // Initialize StreamWriter
+        streamWriter = new StreamWriter(fileName, true);
         rowDataTemp = new string[] { "Time", "Position", "Difficulty", "GameState", "BusEndStation", "BusToTake" };
         rowData = string.Join(",", rowDataTemp);
-        System.IO.File.AppendAllText(fileName, rowData + "\n");
+        streamWriter.WriteLine(rowData);
+        streamWriter.Flush();
     }
 
-    public void WriteToCSV(string time, string position, string gameState, string busEndStation, string busToTake){
+    public void WriteToCSV(string time, string position, string gameState, string busEndStation, string busToTake)
+    {
         rowDataTemp[0] = time;
         rowDataTemp[1] = position;
-        rowDataTemp[2] = "1";//GameSettings.Instance.LevelSelected.ToString();
+        rowDataTemp[2] = GameSettings.Instance.LevelSelected.ToString();
         rowDataTemp[3] = gameState;
         rowDataTemp[4] = busEndStation;
         rowDataTemp[5] = busToTake;
 
         rowData = string.Join(",", rowDataTemp);
-        Debug.Log(fileName);
-        System.IO.File.AppendAllText(fileName, rowData + "\n");
+        streamWriter.WriteLine(rowData);
+        streamWriter.Flush();
+    }
+
+    private void OnDestroy()
+    {
+        // Ensure the StreamWriter is properly closed when the object is destroyed
+        if (streamWriter != null)
+        {
+            streamWriter.Close();
+        }
     }
 }
